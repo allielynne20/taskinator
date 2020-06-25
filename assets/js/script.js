0eb6d88efe33799ef6afa6235e2a3a6678ceb39d
allielynne20 <allielynne20@gmail.com>
2020-06-25 13:30:10 -0400

    added drag and drop styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -215,7 +215,7 @@ var dropZoneDragHandler = function (event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
-        //console.dir(taskListEl);
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -235,7 +235,18 @@ var dropTaskHandler = function (event) {
     else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
     }
+
+    dropZoneEl.removeAttribute("style");
+
     dropZoneEl.appendChild(draggableElement);
+
+};
+
+var dragLeaveHandler = function (event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
 };
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -243,3 +254,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
